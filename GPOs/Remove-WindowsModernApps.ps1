3cb8a4d52382ecf4a36087134c434dbde81941f8
celalettinb-art <celalettinb@gmail.com>
2026-03-03 14:26:39 +0100

    Update Remove-WindowsModernApps.ps1

--- GPOs/Remove-WindowsModernApps.ps1
+++ GPOs/Remove-WindowsModernApps.ps1
@@ -1,11 +1,8 @@
 # Warning: Before executing any script, command, or software obtained from the Internet, ensure that you have carefully reviewed and fully understood its functionality.
 #          Running unverified or untrusted code may result in system damage, data loss, or security breaches.
 # Disclaimer: The author assumes no responsibility or liability for any damage, data loss, or other issues that may arise from the use or misuse of this script or information. Use it at your own risk.
-# Option 1
-# iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/celalettinb-art/windows/refs/heads/main/GPOs/MSEdgeSettings.ps1'))
-# Option 2
-# $url = "https://raw.githubusercontent.com/celalettinb-art/windows/refs/heads/main/GPOs/MSEdgeSettings.ps1"
-# irm $url | iex
+# Run in PS: iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/celalettinb-art/windows/refs/heads/main/GPOs/Remove-WindowsModernApps.ps1'))
+
 
 $apps = @(
 "Clipchamp.Clipchamp",
